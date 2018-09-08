27459d755f0cfe1a6838a545c674accf8ec2bfdb
Kyle Xiao <kylepxiao@gmail.com>
2018-09-08 06:57:42 -0400

    Added delta score

--- wwwroot/js/VSWebApp.js
+++ wwwroot/js/VSWebApp.js
@@ -1,8 +1,6 @@
 ﻿var myVSPingApp = angular.module("myVSPingApp", []);
 myVSPingApp.controller("myController", myControllerFunc);
-myVSPingApp.filter('decodeURI', function ($window) {//Decodes URI's for readability
-    return $window.decodeURI;
-});
+
 myVSPingApp.directive('customOnChange', function () {
     return {
         restrict: 'A',
@@ -22,28 +20,13 @@ console.log("angular initialized");
 function myControllerFunc($scope, $http) {
     console.log("controller");
     $scope.response = null; //Holds the API response
-    $scope.parsedJson = ""; //Holds the response after being parsed into a "pretty" string
-    $scope.msg; //Holds the Loading/Error indicator
-    $scope.url; //Holds the Query URL
-    $scope.market = "en-US"; //Holds the query market
-    $scope.site = null; //Holds the specific domain to search within
-    $scope.showJSONChecked = false; //Holds state of the checkbox
-    $scope.visibility = { //Holds the visibility status of different elements of the page
-        "ImageResults": false,
-        "Tags": false,
-        "JSON": false,
-        "RadioButtons": false
-    };
+
     $scope.activeTable = "VisualSearch"; //Tracks which information is displayed in the main table
     $scope.activeEmotion = 0;
     $scope.player = 0;
     $scope.score = 0;
-    $scope.setVisibility = function (visibility) {//Function to set visibility
-        visibility['ImageResults'] = visibility;
-        visibility['Tags'] = visibility;
-        visibility['JSON'] = visibility;
-        visibility['RadioButtons'] = visibility;
-    };
+    $scope.delta = 0;
+   
 
     $scope.searchFromImg = function () {//Handles sending and recieving API queries of an uploaded image
         var dataURI = window.localStorage.getItem("face.png"); //Selects image (currently we only handle one file at a time)
@@ -54,7 +37,7 @@ function myControllerFunc($scope, $http) {
         }
         var img = new Blob([new Uint8Array(array)], { type: 'image/png' });
 
-        $scope.url = null; //Clears URL box
+       
         if (img === null) {//Terminates if an image isnt selected
             return;
         }
@@ -62,23 +45,10 @@ function myControllerFunc($scope, $http) {
         if ($scope.showJSONChecked) {
             $scope.showJSONChecked = !$scope.showJSONChecked;
         }
-        var domainToFilterResultsTo = $scope.site;//Sets a site to specify results from if it exists
-        var knowledgeRequestPayload = //Creates the body of the request
-            {
-                knowledgeRequest: {
-                    filters: {
-                        site: domainToFilterResultsTo
-                    }
-                },
-                imageInfo: {
-                    cropArea: { top: 0, left: 0, right: 1.0, bottom: 1.0 },
-                    url: null
-                }
-            };
-        $scope.msg = "Loading..."; //Updates loading indicator
+        
+       
         $scope.response = null; //Clears previous response
-        $scope.setVisibility(false); //Hides elements with unloaded information
-
+        
         var formData = new FormData(); //Creates new form object to send to API
         formData.append("image", img); //Add image to request
         //formData.append("knowledgeRequest", JSON.stringify(knowledgeRequestPayload)); Add body to request
@@ -97,54 +67,28 @@ function myControllerFunc($scope, $http) {
                 if (webapiresponse) {
                     var vsResponse = webapiresponse.data; //Recieve response information
                     $scope.response = vsResponse; //Save information to update the page
-                    $scope.parsedJson = getJsonParsed(vsResponse); //Parse response into string
-                    $scope.setVisibility(true); //Makes loaded elements visible again
-                    $scope.msg = null; //Clear loading 
                     console.log($scope.response);
                     if ($scope.activeEmotion == 0) {
-                        $scope.score += vsResponse[0].faceAttributes.emotion.happiness;
+                        $scope.delta = vsResponse[0].faceAttributes.emotion.happiness;
                     } else if ($scope.activeEmotion == 1) {
-                        $scope.score += vsResponse[0].faceAttributes.emotion.anger;
+                        $scope.delta = vsResponse[0].faceAttributes.emotion.anger + 0.5 * vsResponse[0].faceAttributes.emotion.contempt;
                     } else if ($scope.activeEmotion == 2) {
-                        $scope.score += vsResponse[0].faceAttributes.emotion.sadness;
+                        $scope.delta = vsResponse[0].faceAttributes.emotion.sadness + 0.5 * vsResponse[0].faceAttributes.emotion.neutral;
                     } else if ($scope.activeEmotion == 3) {
-                        $scope.score += vsResponse[0].faceAttributes.emotion.surprise;
+                        $scope.delta = vsResponse[0].faceAttributes.emotion.surprise + 0.5 * vsResponse[0].faceAttributes.emotion.fear;
                     }
+                    $scope.score += $scope.delta;
                     if ($scope.player == 1) {
                         document.querySelector("#p1score").innerHTML = parseInt($scope.score * 10);
                     } else {
                         document.querySelector("#p2score").innerHTML = parseInt($scope.score * 10);
                     }
+                    document.querySelector("#deltascorevalue").innerHTML = parseInt($scope.delta * 10);
                 }
-            },
-            function (webapiresponse) { //on failure
-                $scope.msg = "Service Error"; //Update loading indicator
             });
     };
-    $scope.getVisualSearchResults = function (actionType) {//Isolates part of the response for different tables
-        if ($scope.response === null || $scope.response.length === 0) //If there's no response return an empty array
-            return [];
-
-        var vs = $scope.response.faceAttributes.emotion.find(t => t.actionType === actionType);
-        if (vs === undefined) {//If there are no action types, return an empty array
-            return [];
-        }
-        if ($scope.response) {
-            $scope.parsedJson = getJsonParsed($scope.response);
-        }
-        return vs.data.value;
-    };
-    $scope.copyToClipboard = function (name) {//Handles copying the JSON response to the clipboard
-        var copyElement = document.createElement("textarea"); //Creates a copy element
-        copyElement.style.position = 'fixed';
-        copyElement.style.opacity = '0';
-        copyElement.textContent = '' + $scope.parsedJson;
-        var body = document.getElementsByTagName('body')[0];
-        body.appendChild(copyElement);
-        copyElement.select();
-        document.execCommand('copy');
-        body.removeChild(copyElement);
-    };
+    
+    
     var varCounter = 0;
     var intervalId = null;
     $scope.runOneGame = function () {
@@ -154,6 +98,8 @@ function myControllerFunc($scope, $http) {
         var image_url = preview.toDataURL('image/png');
         localStorage.setItem("face.png", image_url);
         $scope.searchFromImg();
+        document.querySelector("#deltascore").style.visibility = "visible";
+        setTimeout(function () { document.querySelector("#deltascore").style.visibility = "hidden" }, 1000);
     };
     $scope.startGame = function () {
         $scope.score = 0;
@@ -162,10 +108,7 @@ function myControllerFunc($scope, $http) {
         }
     }
 }
-function getJsonParsed(json) { //Parses the JSON into a string object
-    var out = JSON.stringify({ json }, null, "\t");
-    return out;
-}
+
 function handleFileSelect(selector) {
     var f = document.getElementById(selector).files[0]; // FileList object   
     var reader = new FileReader();
